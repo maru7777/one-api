8d270c8c9a74f4e8495249d0b78e7cde6f51b4ab
Laisky.Cai <github@laisky.com>
2023-11-17 02:16:17 +0000

    chore: Refactor code and update GitHub workflow
    - Remove comment section from the Footer component in Footer.js
    - Add environment variable `SHORT_SHA` to store the first 8 characters of the commit SHA in ci.yml file
    - Remove the addition of `SHORT_SHA` to the `GITHUB_ENV` file in ci.yml file

--- web/src/components/Footer.js
+++ web/src/components/Footer.js
@@ -43,14 +43,6 @@ const Footer = () => {
             >
               {systemName} {process.env.REACT_APP_VERSION}{' '}
             </a>
-            由{' '}
-            <a href='https://github.com/songquanpeng' target='_blank'>
-              JustSong
-            </a>{' '}
-            构建，源代码遵循{' '}
-            <a href='https://opensource.org/licenses/mit-license.php'>
-              MIT 协议
-            </a>
           </div>
         )}
       </Container>
