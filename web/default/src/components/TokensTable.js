4de5897c715c2c340e7afb94f4ec68771da54c18
Laisky.Cai <github@laisky.com>
2025-03-21 11:54:18 +0000

    fix: remove trailing slashes from API delete endpoint URLs in TokensTable component

--- web/default/src/components/TokensTable.js
+++ web/default/src/components/TokensTable.js
@@ -224,7 +224,7 @@ const TokensTable = () => {
     let res;
     switch (action) {
       case 'delete':
-        res = await API.delete(`/api/token/${id}/`);
+        res = await API.delete(`/api/token/${id}`);
         break;
       case 'enable':
         data.status = 1;
