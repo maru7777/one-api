6f8bedcebdf9dc27c8e3bc92fd416e8452154be4
Laisky.Cai <github@laisky.com>
2025-05-07 02:38:38 +0000

    feat: add gemini-2.5-pro-preview-05-06

--- relay/billing/ratio/model.go
+++ relay/billing/ratio/model.go
@@ -183,9 +183,10 @@ var ModelRatio = map[string]float64{
 	"gemini-2.0-flash-lite-preview-02-05":   0.075 * MilliTokensUsd,
 	"gemini-2.0-flash-thinking-exp-01-21":   0.075 * MilliTokensUsd,
 	"gemini-2.0-flash-exp-image-generation": 0.075 * MilliTokensUsd,
+	"gemini-2.5-flash-preview-04-17":        0.15 * MilliTokensUsd,
 	"gemini-2.0-pro-exp-02-05":              1.25 * MilliTokensUsd,
 	"gemini-2.5-pro-exp-03-25":              1.25 * MilliTokensUsd,
-	"gemini-2.5-flash-preview-04-17":        0.15 * MilliTokensUsd,
+	"gemini-2.5-pro-preview-05-06":          1.25 * MilliTokensUsd,
 	"aqa":                                   1,
 	// https://open.bigmodel.cn/pricing
 	"glm-zero-preview": 0.01 * KiloRmb,
